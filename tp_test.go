ce75b66fc105a6fa4f297f059211e41e30f860eb
sunjian126 <sunjian11@jd.com>
2022-01-25 17:30:16 +0800

    add test

--- tp_test.go
+++ tp_test.go
@@ -21,13 +21,13 @@ func TestTp(t *testing.T) {
 	tt := tp.NewTpTest(tp.NewSpace("test"), 2000000, 1, tp.PrintCallback)
 
 	go func() {
-		time.Sleep(3000)
+		time.Sleep(3000 * time.Millisecond)
 		fmt.Println("to open")
 		tt.SetEnable(true)
-		time.Sleep(3000)
+		time.Sleep(3000 * time.Millisecond)
 		fmt.Println("to close")
 		tt.SetEnable(false)
-		time.Sleep(3000)
+		time.Sleep(3000 * time.Millisecond)
 		fmt.Println("to open")
 		tt.SetEnable(true)
 	}()
@@ -84,6 +84,52 @@ func TestTp2(t *testing.T) {
 		}()
 	}
 
+	wg.Wait()
+	time.Sleep(1000000)
+}
+
+func TestTp3(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		testMap.Store(strconv.Itoa(i), strconv.Itoa(i))
+	}
+
+	tt := tp.NewTpTest(tp.NewSpace("test"), 2000000, 1, tp.PrintCallback)
+	tt.SetEnable(true)
+
+	tt1 := tp.NewTpTest(tp.NewSpace("test1"), 2000000, 1, tp.PrintCallback)
+	tt1.SetEnable(true)
+
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 100000; i++ {
+				tt.Exe(func() {
+					for i := 0; i < 2000; i++ {
+						testMap.Load(strconv.Itoa(i))
+						enable.Load()
+					}
+				})
+			}
+		}()
+	}
+
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 100000; i++ {
+				tt1.Exe(func() {
+					for i := 0; i < 2000; i++ {
+						testMap.Load(strconv.Itoa(i))
+						enable.Load()
+					}
+				})
+			}
+		}()
+	}
 	wg.Wait()
 	time.Sleep(1000000)
 }
